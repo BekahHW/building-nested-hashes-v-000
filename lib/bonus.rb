137896c384439b34d59e6b3779361feb464ddbdc
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-14 11:53:57 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
 
- hero ["Jon Snow"][:status] = "The Lord Commander's Rooms, The Wall, Westeros" 
+ montague ["Romeo"][:status] = "dead" 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
